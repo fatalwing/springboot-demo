cd72506e32bfb203e28961830225c5261f63e5b2
liumeng <liumengying@juniuo.com>
2017-11-08 00:42:43 +0800

    测试类中增加JdbcTemplate用于清理数据等sql操作

--- src/test/java/com/townmc/demo/service/DemoServiceTests.java
+++ src/test/java/com/townmc/demo/service/DemoServiceTests.java
@@ -5,15 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class DemoServiceTests {
+    @Autowired private JdbcTemplate jdbcTemplate;
     @Autowired private DemoService demoService;
 
     @Test
     public void testAdd() {
+        jdbcTemplate.update("delete from demo where demo_id=?", new Object[]{"t2"});
         demoService.add("t2", "自动测试4");
     }
 
