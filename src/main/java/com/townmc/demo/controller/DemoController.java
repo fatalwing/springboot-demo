f68214ae420b4961b216a962d9b42200a7701a6c
liumeng <liumengying@juniuo.com>
2017-11-15 20:35:39 +0800

    controller的demo增加示例

--- src/main/java/com/townmc/demo/controller/DemoController.java
+++ src/main/java/com/townmc/demo/controller/DemoController.java
@@ -5,26 +5,53 @@ import com.townmc.demo.service.DemoService;
 import com.townmc.demo.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/demo")
+@RequestMapping("/_demo_")
 public class DemoController {
 
     @Autowired private DemoService demoService;
 
+    /**
+     * 访问页面
+     * @return
+     */
+    @RequestMapping("/index.htm")
+    public String indexPage() {
+        return "/index";
+    }
+
+
     @RequestMapping("/add.json")
     @ResponseBody
-    public ApiResponse add(String id, String name) {
+    public ApiResponse add(@RequestParam(name="id", required=true) String id,
+                           @RequestParam(name="name", required=false) String name) {
 
         Demo re = demoService.add(id, name);
         return ApiResponse.success(re);
     }
 
-    @RequestMapping("/get.json")
+    /**
+     * 通过post的请求体传参
+     * @param requestBody
+     * @return
+     */
+    @PostMapping("/update.json")
     @ResponseBody
-    public ApiResponse get(String id) {
+    public ApiResponse add(@RequestBody String requestBody) {
+
+        return ApiResponse.success("");
+    }
+
+    /**
+     * 通过rest风格path传参
+     * @param id
+     * @return
+     */
+    @RequestMapping("/{id}/get.json")
+    @ResponseBody
+    public ApiResponse get(@PathVariable("id") String id) {
         Demo re = demoService.findOne(id);
         return ApiResponse.success(re);
     }
