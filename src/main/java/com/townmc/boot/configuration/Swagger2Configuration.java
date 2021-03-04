fe330d95bc8a4f94e3ec544e651a8912cfc82249
liumeng <5192136@qq.com>
2021-03-04 23:08:21 +0800

    升级swagger2到version3.0

--- src/main/java/com/townmc/boot/configuration/Swagger2Configuration.java
+++ src/main/java/com/townmc/boot/configuration/Swagger2Configuration.java
@@ -3,13 +3,14 @@ package com.townmc.boot.configuration;
 import com.townmc.boot.constants.SystemConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.*;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,28 +23,27 @@ import java.util.List;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2Configuration {
+public class Swagger2Configuration implements WebMvcConfigurer {
     private static final String BASE_PACKAGE = "com.townmc.boot";
     private static final String DOC_TITLE = "api文档";
-    private static final String DOC_VERSION = "1.0";
+    private static final String DOC_VERSION = "1.0.0";
 
     @Bean
     public Docket initSwagger() {
+        RequestParameterBuilder aParameterBuilder = new RequestParameterBuilder();
+        aParameterBuilder.name(SystemConstants.AUTHORIZATION_KEY)
+                .query(q -> q.defaultValue(SystemConstants.DEBUG_TOKEN)
+                        .model(modelSpecificationBuilder -> modelSpecificationBuilder.scalarModel(ScalarType.STRING)))
+                .in(ParameterType.HEADER).required(true).build();
 
-        List<Parameter> pars = new ArrayList<Parameter>();
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        tokenPar.name(SystemConstants.AUTHORIZATION_KEY)
-                .defaultValue(SystemConstants.DEBUG_TOKEN)
-                .description("用户访问令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
+        List<RequestParameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
 
         return new Docket(DocumentationType.SWAGGER_2)
-                .globalOperationParameters(pars)
-                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
-                .paths(PathSelectors.any())
-                .build();
+                .paths(PathSelectors.regex("(?!/error.*).*")).build().apiInfo(apiInfo())
+                .globalRequestParameters(aParameters);
     }
 
     private ApiInfo apiInfo() {
@@ -52,4 +52,18 @@ public class Swagger2Configuration {
                 .version(DOC_VERSION)
                 .build();
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler( "/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController( "/swagger-ui/")
+                .setViewName("forward:" +  "/swagger-ui/index.html");
+    }
+
 }
