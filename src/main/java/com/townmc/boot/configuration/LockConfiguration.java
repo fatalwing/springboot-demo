8f134b713e71edb7f5c815728bc14e06bce44f05
lmy <5192136@qq.com>
2022-06-08 17:12:05 +0800

    更新cloud版本

---
+++ src/main/java/com/townmc/boot/configuration/LockConfiguration.java
@@ -0,0 +1,19 @@
+package com.townmc.boot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+
+/**
+ * @author meng
+ */
+@Configuration
+public class LockConfiguration {
+
+    @Bean(destroyMethod = "destroy")
+    public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisLockRegistry(redisConnectionFactory, "lock");
+    }
+
+}
